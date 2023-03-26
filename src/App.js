3000990155ed468104f63f5284fd899eeb1bd209
DEVudg22 <112898483+DEVudg22@users.noreply.github.com>
2023-03-26 13:54:43 -0600

    Update App.js
    Se modificó temporalmente para poner a prueba el componente de captura.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React, { useState } from "react";
 import './App.css';
 import { Login } from "./Login";
 import { Registrar } from "./Registrar";
+import { Capturar } from "./Capturar";
 
 function App() {
   const [formularioActual, setCurrentForm] = useState('login');
+  const [formularioSecundario, setSecundario] = useState('capturar');
 
   const formDos = (nombreForm) => {
     setCurrentForm(nombreForm);
   }
 
+
+
+  
+
   return (
     <div className="App">
+
       {
-        formularioActual === "login" ? <Login onFormSwitch={formDos} /> : <Registrar onFormSwitch={formDos} />
+        formularioActual === "login" ? (<Login onFormSwitch={formDos}  />) : (<Capturar onFormSwitch={formDos} />)
       }
+      
+     
       
     </div>
   );
