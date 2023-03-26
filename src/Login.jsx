0f9ee925276bd67168e927da737100d4b7067ccf
DEVudg22 <112898483+DEVudg22@users.noreply.github.com>
2023-03-26 13:58:13 -0600

    Actualización de componente
    Se le dio función al botón de login para enlazar con el componente de captura,

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { Capturar } from "./Capturar";
 
 export const Login = (props) => {
-    const [correo, setEmail] = useState('');
+    const [numTrabajador, setNumTrabajador] = useState('');
     const [pass, setPass] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(correo);
+        console.log(numTrabajador);
     }
 
     return (
@@ -14,14 +15,14 @@ export const Login = (props) => {
             <h1>SISTEMA DE CAPTURA PENITENCIARIA</h1>
             <h2>INICIO DE SESIÓN</h2>
             <form className="formulario-login" onSubmit={handleSubmit}>
-                <label htmlFor="correo">Correo Electrónico</label>
-                <input value={correo} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="ejemplo@ejemplo.com" id="correo" name="correo" />
+                <label htmlFor="numero-trabajador">Número de Trabajador: </label>
+                <input value={numTrabajador} onChange={(e) => setNumTrabajador(e.target.value)}type="number" placeholder="Su ID" id="numero-trabajador" name="numero-trabajador" />
                 <label htmlFor="password">Contraseña</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Ingresar</button>
+                <button type="submit" onClick={ (e) => props.onFormSwitch('Capturar') }>Ingresar</button>
             </form>
-            <button className="boton-enlace" onClick={() => props.onFormSwitch('register')}>Ir a administrador de usuarios</button>
-            <footer>Versión 1.1</footer>
+            <button className="boton-enlace" onClick={() => props.onFormSwitch('Registrar')}>Ir a administrador de usuarios</button>
+            <footer>Versión 2.1</footer>
         </div>
     )
 }
